dfb5db3036dc2243f9809da5f0e833cc0a3614e7
Marcus <marcuscchan@gmail.com>
2023-03-12 20:17:42 -0700

    Finished filter logic to work with multiple values

--- P02_Final_Submission/Pages/JS/filter.js
+++ P02_Final_Submission/Pages/JS/filter.js
@@ -2,29 +2,57 @@ const galleryContainer = document.querySelector(".gallery-container");
 
 const initialProducts = getInitialProducts();
 
+const activeFilters = new Set();
+
 const filterButtons = document.querySelectorAll(".filter-button");
 filterButtons.forEach((button) => {
   button.addEventListener("click", (e) => {
     const filter = e.target.dataset.filterType;
+    const isActive = e.target.dataset.active;
+    if (isActive) {
+      e.target.removeAttribute("data-active");
+      activeFilters.delete(filter);
+    }
+    else {
+      e.target.setAttribute("data-active", true);
+      activeFilters.add(filter);
+    }
+    if (!activeFilters.size) {
+      galleryContainer.replaceChildren(getGalleryNodes(initialProducts));
+      return;
+    }
     const filteredCategories = initialProducts.map(({ items, title }) => {
       const filteredItems = items.filter((item) => {
-        return item.dataset.filterType === filter
+        const itemFilters = item.dataset.filterType.split(" ");
+        let shouldInclude = true;
+        for (const activeFilter of activeFilters) {
+          if (!itemFilters.includes(activeFilter)) {
+            shouldInclude = false;
+            break;
+          }
+        }
+        return shouldInclude;
       })
-      return { title, filteredItems };
+      return { title, items: filteredItems };
     });
     // Rerender with filtered items
-    const container = document.createDocumentFragment();
-    filteredCategories.forEach((category) => {
-      container.appendChild(category.title);
-      const section = document.createElement("section");
-      section.classList.add("gallery");
-      section.replaceChildren(...category.filteredItems);
-      container.appendChild(section);
-    })
-    galleryContainer.replaceChildren(container);
+    const content = getGalleryNodes(filteredCategories);
+    galleryContainer.replaceChildren(content);
   });
-
 })
+
+function getGalleryNodes(data) {
+  console.log(data);
+  const container = document.createDocumentFragment();
+  data.forEach((category) => {
+    container.appendChild(category.title);
+    const section = document.createElement("section");
+    section.classList.add("gallery");
+    section.replaceChildren(...category.items);
+    container.appendChild(section);
+  })
+  return container;
+}
 
 // Get initial products from the html page
 function getInitialProducts() {
